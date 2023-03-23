419bb23799815d75be16d8eeccd77d9c247419ae
Shenjie <shenjieyouxiang@163.com>
2023-03-23 21:07:49 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://image.axiaozhuo.com/xz/96ba69cd8c6fadd5bfd01d5ad518346b.jpeg',
+      name: '小着',
+      description: '',
     },
   }
 }
